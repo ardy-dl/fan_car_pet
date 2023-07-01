b6f7bf0375c569a256d1ff76b90c30b77bd44960
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-01 11:52:59 +0800

    fan pseudocode

---
+++ fan_class.py
@@ -0,0 +1,22 @@
+# create class named fan
+    # initialization (all private)
+        # speed (df: slow)
+        # radius (df: 5)
+        # color (df: blue)
+        # on (df: false)
+
+    # integer speed
+    # boolean on or off (df: false)
+    # float radius
+    # string color
+    # get speed
+    # set speed
+    # get radius
+    # set radius
+    # get color
+    # set color
+
+# test programs on a diff file
+# fan_1 = max speed, radius 10, yellow, on 
+# fan_2 = medium speed, radius 5, color blue, off
+# display properties 
