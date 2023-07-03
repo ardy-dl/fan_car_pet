94f8b58cddc8993d2028c9d7d8073dc7b170f9a8
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 18:20:53 +0800

    attributes working

--- pet_class.py
+++ pet_class.py
@@ -1,9 +1,19 @@
 # pet class
+class Pet:
     # initialize
+    def __init__(self, name, animal_type, age):
         # name of the pet
+        self.__name = name
         # animal_type
+        self.__animal_type = animal_type
         # age
+        self.__age = age
     # methods
+    def show(self):
+        print(self.__name, self.__animal_type, self.__age)
+
+pet_1 = Pet("callie", "dog", 2)
+pet_1.show()
     # set name
     # set animal type
     # set age
