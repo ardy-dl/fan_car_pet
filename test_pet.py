fecbe6d20f60c3a326da53b5e515f572793dffa3
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 18:49:25 +0800

    the program is working well

--- test_pet.py
+++ test_pet.py
@@ -2,9 +2,17 @@
 from pet_class import Pet
 # create instance of an object
 pet = Pet("", "", 0)
-# prompt the user from attributes and use the accessors to retrieve the attributes 
+# prompt the user for attributes and use the accessors to retrieve the attributes 
 pet_name = str(input("Enter the name of your pet: "))
 pet.set_name(pet_name)
 
+pet_type = str(input("What type of an animal is the said pet? "))
+pet.set_animal_type(pet_type)
+
+pet_age = int(input("Enter the age of your pet (in years): "))
+pet.set_age(pet_age)
+
 # display
 pet.get_name()
+pet.get_animal_type()
+pet.get_age()
