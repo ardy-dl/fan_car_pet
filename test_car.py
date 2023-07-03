8be325a25f7c60a065e3173c877ba5a3fffb7bc6
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 17:16:10 +0800

    new file

---
+++ test_car.py
@@ -0,0 +1,8 @@
+# Testing in a diff file
+car_1 = Car("2021 Mazda3 Turbo", "Mazda", 130)
+car_1.show()
+car_1.accelerate()
+car_1.brake()
+car_1.get_speed()
+# 2021 mazda3 by Mazda
+# 1 car = acc 5x, after EACH call get speed, display. brake x5, after EACH call get speed and display.
