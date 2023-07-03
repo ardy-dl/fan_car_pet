8c3f330666ddad13700c53e073485658d1cc9693
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 19:07:43 +0800

    added a function to add more than 1 pet but not working properly

--- test_pet.py
+++ test_pet.py
@@ -1,18 +1,29 @@
 # import the pet class
 from pet_class import Pet
-# create instance of an object
-pet = Pet("", "", 0)
-# prompt the user for attributes and use the accessors to retrieve the attributes 
-pet_name = str(input("Enter the name of your pet: "))
-pet.set_name(pet_name)
+# if the user has pet greater than 1
+num_pets = int(input("Enter the number of pets you want to add: "))
+pets = []
 
-pet_type = str(input("What type of an animal is the said pet? "))
-pet.set_animal_type(pet_type)
+for i in range(num_pets):
+    print(f"Pet {i+1}: ")
+    # create instance of an object
+    pet = Pet("", "", 0)
+    # prompt the user for attributes and use the accessors to retrieve the attributes 
+    pet_name = str(input("Enter the name of your pet: "))
+    pet.set_name(pet_name)
 
-pet_age = int(input("Enter the age of your pet (in years): "))
-pet.set_age(pet_age)
+    pet_type = str(input("What type of an animal is the said pet? "))
+    pet.set_animal_type(pet_type)
+
+    pet_age = int(input("Enter the age of your pet (in years): "))
+    pet.set_age(pet_age)
+
+    pets.append(pet)
 
 # display
-pet.get_name()
-pet.get_animal_type()
-pet.get_age()
+print("Pet Details:")
+for i, pet in enumerate(pets):
+    print(f"Pet {i+1}:")
+    print("Name:", pet.get_name())
+    print("Animal Type:", pet.get_animal_type())
+    print("Age:", pet.get_age())
