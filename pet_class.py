12b68680bfdee8463adcd81e2c98a70fe48ca318
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 17:33:19 +0800

    pet class pseudocode

---
+++ pet_class.py
@@ -0,0 +1,15 @@
+# pet class
+    # initialize
+        # name of the pet
+        # animal_type
+        # age
+    # methods
+    # set name
+    # set animal type
+    # set age
+    # get name
+    # get animal type
+    # get age 
+
+# new file that creates an object but from user prompt and ask the attributes.
+# use the accessors to retrieve the attributes and display.
