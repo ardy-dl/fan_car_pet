bbd39fffa2ddc08d4e44ee6b5deeec014b06b151
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 17:08:55 +0800

    accelerate method working but not yet polished

--- car_class.py
+++ car_class.py
@@ -10,16 +10,18 @@ class Car:
         self.__speed = speed
 
     def show(self):
-        print(self.__year_model, self.__make, self.__speed)
-       
-
+        print("Year Model:", self.__year_model, "| make:", self.__make, "| speed:", self.__speed)
     # methods
     # accelerate() Add 5 to speed each time it is called
+    def accelerate(self):
+        self.__speed +=5
+        print(self.__speed)
     # brake() subtract 5 to speed
     # get_speed() return current speed
 
 # Testing in a diff file
 car_1 = Car("2021 Mazda3 Turbo", "Mazda", 130)
 car_1.show()
+car_1.accelerate()
 # 2021 mazda3 by Mazda
 # 1 car = acc 5x, after EACH call get speed, display. brake x5, after EACH call get speed and display.
