9a64a4cc1e5252fb8d75d02f53b3aa721e21c110
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 10:30:07 +0800

    Car class pseudocode

---
+++ car_class.py
@@ -0,0 +1,14 @@
+# Car class
+    # initialize
+        # name
+        # Year model
+        # make of the car
+        # current speed (0)
+
+    # methods
+    # accelerate() Add 5 to speed each time it is called
+    # brake() subtract 5 to speed
+    # get_speed() return current speed
+
+# Testing in a diff file
+# 1 car = acc 5x, after EACH call get speed, display. brake x5, after EACH call get speed and display.
