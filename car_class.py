1715119b350bbc4983521b382d63639e1c416392
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 17:11:56 +0800

    brake and get speed is working

--- car_class.py
+++ car_class.py
@@ -16,12 +16,21 @@ class Car:
     def accelerate(self):
         self.__speed +=5
         print(self.__speed)
+        
     # brake() subtract 5 to speed
+    def brake(self):
+        self.__speed -=5
+        print(self.__speed)
+
     # get_speed() return current speed
+    def get_speed(self):
+        print(self.__speed)
 
 # Testing in a diff file
 car_1 = Car("2021 Mazda3 Turbo", "Mazda", 130)
 car_1.show()
 car_1.accelerate()
+car_1.brake()
+car_1.get_speed()
 # 2021 mazda3 by Mazda
 # 1 car = acc 5x, after EACH call get speed, display. brake x5, after EACH call get speed and display.
