4bd53eecafce771466449712a6b7f515bc8fd99e
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-03 18:25:21 +0800

    Get accessors are working

--- pet_class.py
+++ pet_class.py
@@ -4,22 +4,30 @@ class Pet:
     def __init__(self, name, animal_type, age):
         # name of the pet
         self.__name = name
-        # animal_type
+        # animal_type like dog, cat, rabbit etc.
         self.__animal_type = animal_type
         # age
         self.__age = age
     # methods
     def show(self):
         print(self.__name, self.__animal_type, self.__age)
-
-pet_1 = Pet("callie", "dog", 2)
-pet_1.show()
+    # get name
+    def get_name(self):
+        print("The name of the Pet is", self.__name)
+    # get animal type
+    def get_animal_type(self):
+        print("The animal type is", self.__animal_type)
+    # get age 
+    def get_age(self):
+        print("The age of the pet is", self.__age)
     # set name
     # set animal type
     # set age
-    # get name
-    # get animal type
-    # get age 
-
+    
+pet_1 = Pet("callie", "dog", 2)
+pet_1.show()
+pet_1.get_name()
+pet_1.get_animal_type()
+pet_1.get_age()
 # new file that creates an object but from user prompt and ask the attributes.
 # use the accessors to retrieve the attributes and display.
